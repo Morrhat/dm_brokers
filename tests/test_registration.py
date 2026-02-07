98972e57717421d3314f9c8e29e722331d326469
Kristina Pylzy <kovengor@gmail.com>
2026-02-07 14:35:55 +0300

    Added Kafka: Producer

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,8 +1,11 @@
+import json
 import time
 import uuid
+from kafka import KafkaProducer
 
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
+from framework.internal.kafka.producer import Producer
 
 
 def test_failed_registration(account: AccountApi, mail: MailApi) -> None:
@@ -21,6 +24,24 @@ def test_successful_registration(account: AccountApi, mail: MailApi) -> None:
     base = uuid.uuid4().hex
     account.register_user(login=base, email=f"{base}@mail.ru", password="123123123")
 
+    for _ in range(10):
+        response = mail.find_message(query=base)
+        if response.json()["total"] > 0:
+            break
+        time.sleep(1)
+    else:
+        raise AssertionError("Email not found")
+
+#/ui/kafka-cluster/topic
+
+def test_success_registration_with_kafka_producer(mail: MailApi, kafka_producer: Producer) -> None:
+    base = uuid.uuid4().hex
+    message = {
+              "login": base,
+              "email": f"{base}@mail.ru",
+              "password": "123123123"
+            }
+    kafka_producer.send('register-events', message)
     for _ in range(10):
         response = mail.find_message(query=base)
         if response.json()["total"] > 0:
